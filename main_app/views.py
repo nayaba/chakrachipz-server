4b6c93480b6750ec5754fd0854f2e5cd7399e12f
Nabila Ayaba <nabila.ayaba@gmail.com>
2024-01-19 16:16:37 -0500

    Set up base template

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+cupcakes = ['lemon rose', 'vanilla chai', 'chocolate chip cookie dough']
+
 # Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def cupcakes_index(request):
+  return render(request, 'cupcakes.html', {
+    'cupcakes': cupcakes
+  })
